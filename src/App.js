19e588e540717deae686901230f2b6a119a25083
EvanBahr <evanbahrdev@gmail.com>
2023-05-23 09:16:53 +0700

    querying success

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Fragment, useState } from "react";
 import axios from "axios";
+import { handleQueryingUrl } from "./utils";
 
 function App() {
   const [dataTampil, setDataTampil] = useState([]);
   const [pencarian, setPencarian] = useState("Jalan Jalan ke Kota Jakarta");
-  const setCAri = (e) => {
-    setPencarian(e.target.value);
-  };
+  // const [golek, setGolek] = useState({});
 
-  const ambilData = () => {
+  // console.log("query", handleQueryingUrl(golek));
+
+  const ambilData = (filters) => {
     axios
-      .get(`https://api.rekreasi.com/api/activities?filter[title]=${pencarian}`)
+      .get(
+        `https://api.rekreasi.com/api/activities?${handleQueryingUrl(filters)}`
+      )
       .then((res) => {
         // console.log("data ", res.data.data);
         setDataTampil(res.data.data);
@@ -27,23 +30,60 @@ function App() {
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newGolek = {};
+
+    if (e.target.title.value) newGolek.title = e.target.title.value;
+    else delete newGolek.title;
+    if (e.target.minCost.value) newGolek.minCost = e.target.minCost.value;
+    else delete newGolek.minCost;
+
+    if (e.target.maxCost.value) newGolek.maxCost = e.target.maxCost.value;
+    else delete newGolek.maxCost;
+    // console.log("new", newGolek);
+    ambilData(newGolek);
+  };
+
   return (
-    <Fragment>
+    <>
       <div>
-        <header className="App-header max-h-screen p-1 flex">
+        <header className="App-header maxCost-h-screen p-1 flex">
           <img src={logo} className="App-logo h-12" alt="logo" />
           <div className="">React js. prakter membuat filter rekreasi.com</div>
         </header>
       </div>
-      <div className="App grid grid-cols-3 min-h-screen bg-[#282c34] ">
-        <div className="bg-blue-100 col-span-1 p-10 ">
-          <div>nama:</div>
+      <div className="App grid grid-cols-3 minCost-h-screen bg-[#282c34] ">
+        <form onSubmit={handleSubmit} className="bg-blue-100 col-span-1 p-10 ">
+          <div className="text-left font-bold ml-4 text-xl mb-1">title:</div>
           <input
+            name="title"
+            // onChange={(e) => console.log(e.target.value)}
             className="w-full bg-blue-200 px-5 py-6 font-mono font-semibold text-2xl  h-12 rounded-2xl"
-            onChange={setCAri}
-            onKeyDown={cari}
+            // onChange={setCAri}
+            // onKeyDown={cari}
+          />{" "}
+          <div className="text-left font-bold ml-4 text-xl mb-1">
+            minCostim:
+          </div>
+          <input
+            name="minCost"
+            type="number"
+            className="w-full bg-blue-200 px-5 py-6 font-mono font-semibold text-2xl  h-12 rounded-2xl"
+            // onChange={setCAri}
+            // onKeyDown={cari}
+          />{" "}
+          <div className="text-left font-bold ml-4 text-xl mb-1">maxCost:</div>
+          <input
+            name="maxCost"
+            type="number"
+            className="w-full bg-blue-200 px-5 py-6 font-mono font-semibold text-2xl  h-12 rounded-2xl"
+            // onChange={setCAri}
+            // onKeyDown={cari}
           />
-        </div>
+          <button>testse</button>
+        </form>
 
         <div className="grid bg-blue-100 col-span-2 p-10">
           {dataTampil.map((v, i) => (
@@ -64,7 +104,7 @@ function App() {
           ))}
         </div>
       </div>
-    </Fragment>
+    </>
   );
 }
 
